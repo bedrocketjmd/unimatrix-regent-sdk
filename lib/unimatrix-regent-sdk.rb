59762b74b3cfe212a713f3fee6337918be6d7d16
Matthew Yang <yang70@gmail.com>
2016-12-29 13:08:10 -0800

    require added classes in gem index file

--- lib/unimatrix-regent-sdk.rb
+++ lib/unimatrix-regent-sdk.rb
@@ -1,2 +1,7 @@
 require 'unimatrix/regent_sdk/version'
+
 require 'unimatrix/regent_sdk/email_settings_request'
+require 'unimatrix/regent_sdk/configuration'
+require 'unimatrix/regent_sdk/request'
+require 'unimatrix/regent_sdk/response'
+require 'unimatrix/regent_sdk/parser'
